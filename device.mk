bda4a923d8d069e35368d5a8c9f89ac1e064630a
dpenra <dpenra.stha@gmail.com>
2024-09-18 10:34:01 +0545

    sunny: added gallery and bokeh support
    this adds miui gallery and bokehs to the system which enables to edit potrait image
    
    sunny: added gallery and bokeh support
    
    this adds miui gallery and bokehs to the system which enables to edit potrait image

--- device.mk
+++ device.mk
@@ -554,3 +554,6 @@ $(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
 
 # BCR
 $(call inherit-product, vendor/bcr/bcr.mk)
+
+# Miui-apps
+$(call inherit-product, vendor/xiaomi/miuiapps/config.mk)
